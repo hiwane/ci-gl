0725fdd84588c265d076e2660e05fe18b0be352a
hiwane <hiwane@users.noreply.github.com>
2018-08-30 22:02:33 +0900

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-curl --trace-ascii dump.txt http://localhost:10080/
-cat dump.txt
-curl --request POST http://localhost:10080/oauth/token
-curl --request POST --data '{"grant_type": "password", "username": "root", "password": "5iveL!fe"}' http://localhost:10080/oauth/token
+for port in 80 10080 8080
+do
+  curl --trace-ascii dump.txt http://localhost:${port}/
+  cat dump.txt
+  curl --request POST http://localhost:10080/oauth/token
+  curl --request POST --data '{"grant_type": "password", "username": "root", "password": "5iveL!fe"}' http://localhost:${port}/oauth/token
+done
 exit $?
